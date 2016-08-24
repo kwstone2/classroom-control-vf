22b852fd044f32beedadc3f0bf849a95e20f60b7
kwstone2 <kwstx@yahoo.com>
2016-08-24 15:33:31 -0500

    Trying to find a problem

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -12,7 +12,7 @@ class skeleton {
       group   => 'root',
       mode    => '0644',
    }
-   ->
+   
    file { 'bashrc':
       path    => '/etc/skel/.bashrc',
       source  => 'puppet:///modules/skeleton/bashrc',
