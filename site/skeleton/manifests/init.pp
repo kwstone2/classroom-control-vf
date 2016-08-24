4244f57b0f0599e6ac5310aeabf447eec04e0331
kwstone2 <kwstx@yahoo.com>
2016-08-24 15:16:44 -0500

    Skeleton .bashrc work

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,20 @@
+class skeleton {
+   file { '/etc/skel':
+      ensure  => directory,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+   }
+   
+   file { '/etc/skel/.bashrc':
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+   }
+   ->
+   file_line { 'Append to skel .bashrc':
+      path    => '/etc/skel/.bashrc',
+      line    => '/bin/echo I'm skeleton in the closet
+   }
+}
