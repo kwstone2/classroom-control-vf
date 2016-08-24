c753557186e63d16493b0eb1362e327924a72c68
kwstone2 <kwstx@yahoo.com>
2016-08-24 17:55:30 -0500

    Initial cut at nginx init.pp manifest

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,18 @@
+class nginx {
+  package { 'nginx':
+    ensure  => present,
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    owner   => ,
+    group   => ,
+    require => Package['nginx'],
+    mode    => '0644',
+  }
+  
+  service { 'nginx':
+    subscribe => File['/etc/nginx/nginx.conf'],
+    ensure    => running,
+  }
+}
