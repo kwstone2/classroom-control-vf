24dca7a75d81484c672c0aaed30da20a2466eb89
kwstone2 <kwstx@yahoo.com>
2016-08-24 18:10:15 -0500

    More work on it

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,10 +5,35 @@ class nginx {
 
   file { '/etc/nginx/nginx.conf':
     ensure  => file,
-    owner   => ,
-    group   => ,
+    owner   => root,
+    group   => root,
     require => Package['nginx'],
     mode    => '0644',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+  }
+  
+  file { '/var/www':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+  
+  file { '/var/www/index.html':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/nginx/default.conf',
+  
   }
   
   service { 'nginx':
