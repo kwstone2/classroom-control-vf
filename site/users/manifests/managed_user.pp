c85ef9bb9a3cca4811ec03ef7aef1fa7f434b72a
kwstone2 <kwstx@yahoo.com>
2016-08-25 15:55:26 -0500

    incomplete

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,22 @@
+define users::managed_user (
+  $group = $title,
+)
+{
+  user { $title:
+     ensure = present,
+  }
+ 
+ file { "/home/${title}":
+   ensure => directory,
+   owner  => $title,
+   group  => $group,
+ }
+
+ file { "/home/${title}/.ssh":
+   ensure => directory,
+   owner  => $title,
+   group  => $group,
+   mode   => '0700',
+  }
+   
+}
