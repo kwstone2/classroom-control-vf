6dcd9bbb3be3065f655aaff444879e27aa171cbf
kwstone2 <kwstx@yahoo.com>
2016-08-25 12:09:11 -0500

    Code simplification

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,32 @@
 class nginx {
+
+  File {
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    ensure => file,
+  }
+
   package { 'nginx':
     ensure  => present,
   }
 
   file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
     require => Package['nginx'],
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
   }
   
   file { '/var/www':
     ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
     mode    => '0755',
   }
   
   file { '/var/www/index.html':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/index.html',
   }
   
   file { '/etc/nginx/conf.d/default.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/default.conf',
-  
   }
   
   service { 'nginx':
