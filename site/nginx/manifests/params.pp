7724aa9749a253dd475dbcd0f509981dbb6a755d
kwstone2 <kwstx@yahoo.com>
2016-08-26 13:40:12 -0500

    Broke out params to separate pp

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,35 @@
+class nginx::params {
+
+  case $::os['family'] {
+    'redhat', 'debian' : {
+      $pkgname   = 'nginx'
+      $fileowner = 'root'
+      $filegroup = 'root'
+      $def_docroot  = '/var/www'
+      $configdir = '/etc/nginx'
+      $srvblkdir = '/etc/nginx/conf.d'
+      $logdir    = '/var/log/nginx'
+      $svcname   = 'nginx'
+    }
+    'windows' : {
+      $pkgname   = 'nginx-service'
+      $fileowner = 'Administrator'
+      $filegroup = 'Administrators'
+      $def_docroot  = 'C:/ProgramData/nginx/html'
+      $configdir = 'C:/ProgramData/nginx'
+      $srvblkdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir    = 'C:/ProgramData/nginx/logs'
+      $runsas    = 'nobody'
+      $svcname   = 'nginx' 
+    }
+    default : {
+      fail("Module ${module_name} is not supported on ${os['family']}" )
+    }
+  }
+  
+    $runsas = $::os['family'] ? {
+    'RedHat' => 'nginx',
+    'Debian' => 'www-data',
+  }
+
+}
